9e899a912b40fbd0ea4dadbbccddbdff43a24253
jejikeh <44530103+jejikeh@users.noreply.github.com>
2022-04-05 01:07:42 +0300

    replace Quake3CharacterRotation to Quake3Rotation

--- Assets/Quake3 Style Movenment/Scripts/Quake3Player.cs
+++ Assets/Quake3 Style Movenment/Scripts/Quake3Player.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 namespace Quake3MovementStyle
 {
 
+    [RequireComponent(typeof(Quake3Rotation))]
     public class Quake3Player : MonoBehaviour
     {
 
         [SerializeField] private Transform _character;
         [SerializeField] private Transform _camera;
         [SerializeField] private Quake3Rotation _characterRotation;
+
+
+        private Vector2 _mouseInput; // Mouse 2D Input Vector
 
 
         void Start()
@@ -21,7 +24,8 @@ namespace Quake3MovementStyle
 
         void Update()
         {
-            _characterRotation.LookRotation(_character, _camera, Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            _mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); // Mouse Input Vector
+            _characterRotation.LookRotation(_character, _camera,_mouseInput.x,_mouseInput.y);
         }
     }
 }
