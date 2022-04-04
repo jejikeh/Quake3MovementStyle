27d3755d48bea4281571da48ad3b4f2497b3517a
jejikeh <44530103+jejikeh@users.noreply.github.com>
2022-04-04 10:37:03 +0300

    Fix names and add Math.Clamp

--- Assets/Quake3 Style Movenment/Scripts/Quake3 Base Scripts/Quake3CharacterRotation.cs
+++ Assets/Quake3 Style Movenment/Scripts/Quake3 Base Scripts/Quake3CharacterRotation.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Quake3CharacterRotation : MonoBehaviour
 {
     [SerializeField] private float _rotateSpeed;
-    [SerializeField] private float _smoothRotateSpeed;
+    [SerializeField] private float _smoothRotationSpeed;
+    [SerializeField] private float _limitAngle;
 
     private Vector2 _rotation;
 
@@ -15,21 +14,23 @@ public class Quake3CharacterRotation : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-        } else
+        }
+        else
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
     }
 
-    public void LookRotation(Transform character, Transform camera,float xRotation,float yRotation)
+    public void LookRotation(Transform character, Transform camera, float xRotation, float yRotation)
     {
-        _rotation.x += xRotation * _rotateSpeed; 
+        _rotation.x += xRotation * _rotateSpeed;
         _rotation.y += yRotation * _rotateSpeed;
+        _rotation.y = Mathf.Clamp(_rotation.y, -_limitAngle, _limitAngle);
         var xQuaternion = Quaternion.AngleAxis(_rotation.x, Vector3.up);
         var yQuaternion = Quaternion.AngleAxis(_rotation.y, Vector3.left);
 
-        camera.localRotation = Quaternion.Slerp(camera.localRotation, yQuaternion, _smoothRotateSpeed * Time.deltaTime);
-        character.localRotation = Quaternion.Slerp(character.localRotation, xQuaternion, _smoothRotateSpeed * Time.deltaTime);
+        camera.localRotation = Quaternion.Slerp(camera.localRotation, yQuaternion, _smoothRotationSpeed * Time.deltaTime);
+        character.localRotation = Quaternion.Slerp(character.localRotation, xQuaternion, _smoothRotationSpeed * Time.deltaTime);
     }
 }
