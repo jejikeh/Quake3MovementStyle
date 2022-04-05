c140c5cf5d056cd691badeb4109d96d8ff90a8d3
jejikeh <44530103+jejikeh@users.noreply.github.com>
2022-04-05 15:15:44 +0300

    movement on the ground and gravity

---
+++ Assets/Quake3 Style Movenment/Scripts/Quake3 Base Scripts/Quake3Movement.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Quake3MovementStyle
+{
+    //[RequireComponent(typeof(CharacterController))]
+    public class Quake3Movement : MonoBehaviour
+    {
+
+        private Vector3 _characterVelocity = Vector3.zero;
+
+
+        [System.Serializable]
+        private class _movementSettings
+        {
+            public float MaxSpeed;
+            public float Acceleration;
+            public float Deceleration;
+
+            public _movementSettings(float maxSpeed, float acceleration, float deceleration)
+            {
+                MaxSpeed = maxSpeed;
+                Acceleration = acceleration;
+                Deceleration = deceleration;
+            }
+        }
+
+        //private float _speed { get { return _characterController.velocity.magnitude;  } }
+        [SerializeField] private _movementSettings _groundMovementSettings = new _movementSettings(0, 0, 0);
+
+        
+        
+        [SerializeField] private float _friction;
+        [SerializeField] private float _gravity;
+
+
+        public void Movement(CharacterController characterController,Transform characterTransform ,Vector3 directionInput)
+        {
+            if (characterController.isGrounded)
+            {
+                GroundMove(characterTransform, directionInput.x, directionInput.z);
+            } else
+            {
+                AirMove();
+            }
+            characterController.Move(_characterVelocity * Time.deltaTime);
+
+        }
+
+        private void GroundMove(Transform characterTransform,float xInput, float zInput)
+        {
+            ApplyFriction(1.0f);
+            Vector3 wishDirection = new Vector3(xInput, 0, zInput);
+            wishDirection = characterTransform.TransformDirection(wishDirection);
+
+            float wishSpeed = wishDirection.magnitude;
+            wishSpeed *= _groundMovementSettings.MaxSpeed;
+            Accelerate(wishDirection, wishSpeed, _groundMovementSettings.Acceleration);
+        }
+
+        private void AirMove()
+        {
+            _characterVelocity.y -= _gravity * Time.deltaTime;
+        }
+
+        private void Accelerate(Vector3 targetDirection, float targetSpeed, float acceleration) // Float acceleration can be removed.
+        {
+            float currentSpeed = Vector3.Dot(_characterVelocity, targetDirection);
+
+            if ( (targetSpeed - currentSpeed) <= 0)
+            {
+                return;
+            }
+
+            float accelerationSpeed = acceleration * targetSpeed * Time.deltaTime;
+            if (accelerationSpeed > (targetSpeed - currentSpeed))
+            {
+                accelerationSpeed = (targetSpeed - currentSpeed);
+            }
+
+            _characterVelocity.x += accelerationSpeed * targetDirection.x;
+            _characterVelocity.z += accelerationSpeed * targetDirection.z;
+        }
+
+        private void ApplyFriction(float t)
+        {
+
+            //_characterVelocity.y = 0;
+            float speed = _characterVelocity.magnitude;
+            float drop = 0;
+            // Checking if grounded
+
+            float control = speed < _groundMovementSettings.Deceleration ? _groundMovementSettings.Deceleration : speed;
+            drop = control * _friction * t * Time.deltaTime;
+
+
+            float newSpeed = speed - drop;
+
+            if (newSpeed < 0)
+            {
+                newSpeed = 0;
+            }
+            if (newSpeed > 0)
+            {
+                newSpeed /= speed;
+            }
+            _characterVelocity.x *= newSpeed;
+            _characterVelocity.z *= newSpeed;
+
+        }
+    }
+
+}
--- Assets/Quake3 Style Movenment/Scripts/Quake3Player.cs
+++ Assets/Quake3 Style Movenment/Scripts/Quake3Player.cs
@@ -5,27 +5,43 @@ namespace Quake3MovementStyle
 {
 
     [RequireComponent(typeof(Quake3Rotation))]
+    [RequireComponent(typeof(Quake3Movement))]
+    [RequireComponent(typeof(CharacterController))]
     public class Quake3Player : MonoBehaviour
     {
+        [SerializeField] private Transform _characterTransform;
 
-        [SerializeField] private Transform _character;
+
+        [Header("Camera")]
         [SerializeField] private Transform _camera;
         [SerializeField] private Quake3Rotation _characterRotation;
-
-
         private Vector2 _mouseInput; // Mouse 2D Input Vector
 
 
-        void Start()
+        [Header("Movement")]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Quake3Movement _characterMovement;
+        private Vector3 _keyboardInput;
+
+
+        private void Start()
         {
+            _characterController = GetComponent<CharacterController>();
+
             _characterRotation = GetComponent<Quake3Rotation>();
             _characterRotation.SetCursorLock(true);
         }
 
+
         void Update()
         {
             _mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); // Mouse Input Vector
-            _characterRotation.LookRotation(_character, _camera,_mouseInput.x,_mouseInput.y);
+            _characterRotation.LookRotation(_characterTransform, _camera,_mouseInput.x,_mouseInput.y);
+
+            _keyboardInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            _characterMovement.Movement(_characterController, _characterTransform, _keyboardInput);
+            
+
         }
     }
 }
